708442def0ad755969ca528085975e7c44cde51d
codetobuild <goodnakha@gmail.com>
2022-01-07 13:32:26 +0530

    redirect file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import SavedCharacters from "./Components/SavedCharacters";
 
-
 function App() {
   return (
     <BrowserRouter>
